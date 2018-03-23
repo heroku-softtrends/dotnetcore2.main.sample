eba51c89048a626f4cc27e4cf2c77e05282dd4b6
Somashekar G Achar <somashekarg@softtrends.com>
2018-03-23 15:07:57 +0530

    netcore main

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcoremain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
